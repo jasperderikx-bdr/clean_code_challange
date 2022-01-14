ff80dfc1af84b7129203a1973709673f0e1d14d1
Your Name <jasper.derikx@vantage-ai.com>
2022-01-14 02:44:01 +0100

    make code dirty

--- clean_code_guidelines.md
+++ clean_code_guidelines.md
@@ -14,22 +14,23 @@
 -	Replace conditions (in if- or loop-statements) by functions.
 -	No repetition or duplication of code.
 -   Use type annotations (enforce them with mypy).
+-   Use 1 language (English).
 
 ### Functions
 -	Small. How small? 
      - Do one thing.
      - All statements are of the same level of abstraction. One level lower than the function itself.
      - Keep extracting methods until the only thing left to extract is the function itself.
--	It will automatically turn out to roughly 7 lines max.
+-	This will automatically result to roughly 7 lines max.
 -	Prefer fewer arguments. Use classes, or data structures to reduce the number of arguments.
--	Have no side effects. (Like saving files?)
--	When calling functions, write all arguments explicitly.
+-	Have no side effects.
+-	Prefer keyword arguments over positional arguments in function calls.
 
 ### Comments
 -	Express yourself in code first.
 -	Only if the code can’t describe its purpose, use comments to clarify the intent of the code.
 -	Don’t be redundant or add obvious noise.
--	Don’t comment out code, we have git for this.
+-	Don’t comment out code, we have git for version control.
 -	Docstrings and arguments description, only if necessary. 
 
 ### Style Guide
@@ -38,9 +39,8 @@
 
 ### Imports
 -	All imports at the top.
--	No relative imports.
--	Order them alphabetically and first in modules, sub-imports, project imports.
--	Use isort to enforce this.
+-	No relative imports or wildcards.
+-	Sort imports in a standard way (use isort to automate this.)
 
 ### Tests
 -	One assert per test.
--- main.py
+++ main.py
@@ -1,57 +1,84 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression, RidgeCV
-from datetime import time, datetime
 
-supplies = ["pan", "rasp", "kom"]
+from datetime import *
 
-
-def clean(text):
-    str_list = text.split()
-    str_list = [''.join(e for e in str if e.isalnum()) for str in str_list]
-    str_list = [str.lower() for str in str_list]
-    return str_list
+supply_words = ["pan", "rasp", "kom"]
 
 
-def parse_recipes():
-    df = pd.read_csv("data/lunch_recipes.csv")
-    for word in supplies:
-        df[f"{word}"] = df.recipe.apply(lambda text: clean(text).count(word) > 0)
-    df = df.drop(['dish', 'url', "servings", "recipe"], axis=1)
+
+
+
+def getRecipesDF():
+    df = pd.read_csv("data/lunch_recipes.csv")  # Read lunch recipes dataframe.
+    for wrd in supply_words:
+        def hulp_clean(text):
+            # This function cleans text by seperating all the words and removing punctution
+            #
+            # text:str
+
+            str_list = [''.join(O for O in str if O.isalnum()) for str in text.split()]
+            str_list = [str.lower() for str in str_list]
+            return str_list
+
+        df[f"{wrd}"] = df.recipe.apply(lambda text: hulp_clean(text).count(wrd) >0) ## count the amount of times a word occurs in the recipe.
+        df[f"{wrd}"] = df[f"{wrd}"].apply(lambda x: x is True)
+    df = df.drop('servings', axis= 1)
+    df = df.drop('recipe', axis= 1)
+    df['date'] = df.date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    df = df.drop("url", axis= 1)
+    df = df.drop("dish", axis= 1)
+
+
     return df
 
-
-def parse_attendance_sheet():
-    df = pd.read_csv("data/key_tag_logs.csv")
-    df['timestamp'] = df.timestamp.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
-    df['date'] = df.timestamp.apply(lambda x: x.date())
-    df['time'] = df.timestamp.apply(lambda x: x.time())
+def attendance_sheet_uitlezen():
+    df = pd.read_csv("../clean_code/data/key_tag_logs.csv")
+    df['timestamp2'] = df.timestamp.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+    df['date'] = df.timestamp.apply(lambda x: datetime.strptime(x[:10], '%Y-%m-%d'))
+    df['time'] = df.timestamp2.apply(lambda x: x.time())
+    df['timestamp'] = df['timestamp2']
+    df = df.drop('timestamp2', axis=1)
     import numpy as np
     result = pd.DataFrame(np.array(df.date), columns=['date']).drop_duplicates()
 
     # print(df.name.unique())
     for name in df.name.unique():
-        df2 = df[df.name == name]
+        lunchdates = []
+        for datum in df.date.unique():
+            df2 = df[df.name == name]
+            df2 = df2[df2.date == datum]
 
-        df_check_in = df2[df2.event == "check in"]
-        df_check_in = df_check_in[df_check_in.time < time(12, 0, 0)]
+            dataframe_check_in = df2[df2.event == "check in"]
+            dataframe_check_in = dataframe_check_in[dataframe_check_in.time < time(12,0, 0)]
 
-        df_check_out = df2[df2.event == "check out"]
-        df_check_out = df_check_out[df_check_out.time > time(12, 0, 0)]
-        lunch_dates = df_check_in.merge(df_check_out, on="date", how="inner").date
-        result[f"{name}"] = result.date.apply(lambda x: 1 if x in list(lunch_dates) else 0)
+            df_check_out = df2[df2.event == "check out"]
+            df_check_out = df_check_out[df_check_out.time > time(12, 0, 0)]
+            if df_check_out.shape[0] > 0 and dataframe_check_in.shape[0] > 0:
+                lunchdates.append(datum)
 
-    result['date'] = result['date'].apply(str)
+        result[f"{name}"] = result.date.apply(lambda x: 1 if x in list(lunchdates) else 0)
+
+    result['date'] = result['date']#.apply(str)
     return result
 
+def train_model(alpha=0.1):
+    recipes = getRecipesDF()
+    attendance = attendance_sheet_uitlezen()
+    l=pd.read_csv("data/dishwasher_log.csv")
+    l["date"] = l.date.apply(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
-def train_model():
-    recipes = parse_recipes()
-    attendance = parse_attendance_sheet()
-    df = recipes.merge(attendance, on="date", how="outer").merge(pd.read_csv("data/dishwasher_log.csv")).fillna(0)
-    reg = LinearRegression().fit(df.drop(["dishwashers", "date"], axis=1), df["dishwashers"])
+    df = recipes.merge(attendance,
+                    on="date",
+                       how="outer").merge(l).fillna(0)
+    reg = LinearRegression(fit_intercept=False, positive=True)\
+          .fit(df.drop(["dishwashers", "date"], axis=1),
+            df["dishwashers"])
     return dict(zip(reg.feature_names_in_,
-                    [round(c, 3) for c in reg.coef_]))
+            [round(c, 3) for c in reg.coef_]))
+
 
 
 if __name__ == "__main__":
+
     print(train_model())
